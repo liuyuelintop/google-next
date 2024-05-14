428ccecb3223b4fc26e00210e6cd59c4d14a040a
liuyuelintop <liuyuelintop@gmail.com>
2024-05-14 23:15:01 +1000

    Edit Web Search Page and Image Search Page

--- src/app/search/image/page.jsx
+++ src/app/search/image/page.jsx
@@ -1,20 +1,20 @@
 import ImageSearchResults from '@/components/ImageSearchResults';
 import Link from 'next/link';
-import React, { Suspense } from 'react';
-import Loading from './loading';
 
-const _ImageSearchPage = async ({ searchParams }) => {
+async function fetchSearchResults(searchParams) {
     const startIndex = searchParams.start || '1';
-    await new Promise((resolve) => setTimeout(resolve, 1000));
-
     const response = await fetch(
         `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&searchType=image&start=${startIndex}`
     );
     if (!response.ok) throw new Error('Something went wrong');
     const data = await response.json();
-    const results = data.items;
+    return data.items;
+}
 
-    if (!results) {
+export default async function ImageSearchPage({ searchParams }) {
+    const results = await fetchSearchResults(searchParams);
+
+    if (!results || results.length === 0) {
         return (
             <div className='flex flex-col justify-center items-center pt-10'>
                 <h1 className='text-3xl mb-4'>
@@ -30,13 +30,5 @@ const _ImageSearchPage = async ({ searchParams }) => {
         );
     }
 
-    return <div>{results && <ImageSearchResults results={data} />}</div>;
-}
-
-export default function ImageSearchPage(props) {
-    return (
-        <Suspense fallback={<Loading />}>
-            <_ImageSearchPage {...props} />
-        </Suspense>
-    );
+    return <ImageSearchResults results={results} />;
 }
--- src/app/search/web/page.jsx
+++ src/app/search/web/page.jsx
@@ -1,18 +1,20 @@
 import WebSearchResults from '@/components/WebSearchResults';
 import Link from 'next/link';
-import { React, Suspense } from 'react';
-import Loading from './loading';
-const _WebSearchPage = async ({ searchParams }) => {
-    const startIndex = searchParams.start || '1';
-    await new Promise((resolve) => setTimeout(resolve, 1000));
 
+async function fetchWebSearchResults(searchParams) {
+    const startIndex = searchParams.start || '1';
     const response = await fetch(
-        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}'}&start=${startIndex}`
+        `https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}&cx=${process.env.CONTEXT_KEY}&q=${searchParams.searchTerm}&start=${startIndex}`
     );
     if (!response.ok) throw new Error('Something went wrong');
     const data = await response.json();
-    const results = data.items;
-    if (!results) {
+    return data.items;
+}
+
+export default async function WebSearchPage({ searchParams }) {
+    const results = await fetchWebSearchResults(searchParams);
+
+    if (!results || results.length === 0) {
         return (
             <div className='flex flex-col justify-center items-center pt-10'>
                 <h1 className='text-3xl mb-4'>
@@ -27,13 +29,6 @@ const _WebSearchPage = async ({ searchParams }) => {
             </div>
         );
     }
-    return <div>{results && <WebSearchResults results={data} />}</div>;
-}
 
-export default function WebSearchPage(props) {
-    return (
-        <Suspense fallback={<Loading />}>
-            <_WebSearchPage {...props} />
-        </Suspense>
-    );
+    return <WebSearchResults results={results} />;
 }
