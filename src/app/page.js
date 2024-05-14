3e76398939ff989201f537e21d66f0381094cb61
liuyuelintop <liuyuelintop@gmail.com>
2024-05-14 15:57:47 +1000

    add body section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,22 @@
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
 import React from "react";
 
 export default function Home() {
   return (
     <>
       <HomeHeader />
+      <div className="flex flex-col items-center mt-24">
+        <Image
+          src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          alt="Google Logo"
+          width={272}
+          height={92}
+          priority
+        />
+        <HomeSearch />
+      </div>
     </>
   );
 }
